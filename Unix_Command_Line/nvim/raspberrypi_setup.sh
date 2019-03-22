c89f110a2b991d65f2d8731e3049f4d9b1ff84da
Kauna Lei <kaunalei@gmail.com>
2019-03-22 00:32:02 -0700

    add .gitignore and expand raspberrypi_setup script
    adding sections to raspberrypi_setup.sh for:
    - updating init.vim for nvim
    - vim plug setup lines
    - instructions for vim plug setup

--- Unix_Command_Line/nvim/raspberrypi_setup.sh
+++ Unix_Command_Line/nvim/raspberrypi_setup.sh
@@ -10,3 +10,18 @@ cd neovim
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
 pip3 install neovim
+
+wget --quiet https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage --output-document nvim
+
+chmod +x nvim 
+sudo chown root:root: nvim
+sudo mv nvim /usr/bin
+
+mkdir -p ~/.config/nvim
+cp ./my-rpi-init.vim ~/.config/nvim/init.vim
+
+# vimplug setup
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "next open nvim and type :PlugInstall to install plugins"
