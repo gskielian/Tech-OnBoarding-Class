3229a2eb7981059888aa9f7bc43be37169999c18
Kauna Lei <kaunalei@gmail.com>
2019-06-09 13:01:17 -0700

    Add simple inverter example.

---
+++ FPGA/Lattice/Ice40/01_simple_inverter/simple_inverter.v
@@ -0,0 +1,20 @@
+// Some module organizational notes shared in comments
+
+module simple_inverter
+   (
+      // List Inputs
+      input_1,
+
+      // List Outputs
+      output_1
+      );
+
+	// Define Port(s)
+	input input_1;
+
+	output output_1;
+
+	// Define Connection(s)
+	assign output_1 = ~input_1;
+
+endmodule
