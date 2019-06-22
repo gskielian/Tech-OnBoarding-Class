2004b750858e5150d780dec05ae3bece51fe1d70
Kauna Lei <kaunalei@gmail.com>
2019-06-21 17:34:12 -0700

    update setup script for kicad 5.1

--- EE/KiCad/setup.sh
+++ EE/KiCad/setup.sh
@@ -2,10 +2,10 @@
 
 # ubuntu setup
 
-sudo add-apt-repository --yes ppa:js-reynaud/kicad-5
+sudo add-apt-repository --yes ppa:js-reynaud/kicad-5.1
 sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install -y kicad
+sudo apt-get upgrade -y 
+sudo apt-get install -y --install-suggests kicad
 
 sudo apt install libcanberra-gtk-module libcanberra-gtk3-module
 
