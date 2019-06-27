231b6cdad0857ae633039cbf2b2d90a874210172
Kauna Lei <kaunalei@gmail.com>
2019-06-26 21:37:59 -0700

    add on and off examples to pi gpio folder

---
+++ Embedded_Linux/raspberry_pi/peripherals/gpio/01_toggle_on_gpio.py
@@ -0,0 +1,16 @@
+
+#!/usr/bin/python3
+
+import RPi.GPIO as GPIO
+import time
+
+
+# use bcm numbering as opposed to position number
+GPIO.setmode(GPIO.BCM)
+
+# set mode to output
+GPIO.setup(4, GPIO.OUT)
+
+# toggle GPIO_4 (on B+ this is Pin Position #7)
+
+GPIO.output(4, True)
