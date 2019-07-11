e8455d5d8fde18c878dd02db21dfac8463b66bf0
klei22 <kaunalei@gmail.com>
2019-07-11 06:48:55 +0000

    update template program to resemble 01_blinky_led
    using blinky_led for template instead of combinational example as this
    has good placeholders for clock signals and clock dividers.

--- FPGA/Lattice/IceBreaker/.template/template_program.v
+++ FPGA/Lattice/IceBreaker/.template/template_program.v
@@ -1,19 +1,29 @@
-// module description: 
-// completely parallel logic
+// module description
+//
+//
 
 module template_program (
+	input CLK,
 	input  BTN1, BTN2, BTN3,
 	output LED1, LED2, LED3, LED4, LED5
 );
 
+reg [19:0] clock_divider = 0;
+reg clock_pulse = 0;
 
-// Simple Logic with Buttons and LEDs
-	assign LED1 = BTN1 && BTN2;
-	assign LED2 = BTN1 || BTN2;
-	assign LED3 = BTN1 ^ BTN3;
-	assign LED4 = !BTN3;
-	assign LED5 = BTN1 && BTN2 && BTN3;
+always @(posedge CLK) begin
+	if (clock_divider == 500000) begin
+		clock_divider <= 0;
+		clock_pulse <= 1;
+	end else begin
+		clock_divider <= clock_divider + 1;
+		clock_pulse <= 0;
+	end
 
+	if (clock_pulse) begin 
+		LED1 <= ~LED1;
+	end
+end
 
 endmodule
 
