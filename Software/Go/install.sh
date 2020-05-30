a2801cd817df42d65dcddd3992f074be24264047
Kauna <kaunalei@gmail.com>
2020-05-30 15:27:00 -0700

    add install script for go

---
+++ Software/Go/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "vim into this install file and uncomment the correct install for your version"
+
+# for ubuntu 20
+#sudo apt install golang-1.14-go
+
+# for ubuntu 18.04 or 19.10
+#sudo add-apt-repository ppa:longsleep/golang-backports
+#sudo apt update
+#sudo apt install golang-go
